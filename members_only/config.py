53918426497b57dcb0287aec3c32a1eb852318d1
Max <teratoscincus@hotmail.com>
2022-11-02 15:25:46 +0100

    Rename BASE_DIR and add APP_DIR constants
    Rename BASE_DIR to ROOT_DIR as is points to the porjects root directory.
    APP_DIR points to the directory of the application.

--- members_only/config.py
+++ members_only/config.py
@@ -5,11 +5,20 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
+ROOT_DIR = Path().parent.resolve()
+APP_DIR = ROOT_DIR / "members_only"
+
 # Environmental variables
-DOTENV_PATH = Path().parent.resolve() / ".env"
+DOTENV_PATH = APP_DIR / ".env"
 load_dotenv(DOTENV_PATH)
 
 # Development config
 FLASK_ENV = "development"
 DEBUG = True
 SECRET_KEY = os.environ.get("SECRET_KEY")
+
+# Development database
+DB_DIR = APP_DIR / "database"
+DATABASE = "db.sqlite3"
+DATABASE_PATH = DB_DIR / DATABASE
+SCHEMA_PATH = DB_DIR / "schema.sql"
